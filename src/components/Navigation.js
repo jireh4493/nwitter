a63f50ee73b4fab1b3494b1d49c99269704e0f82
jireh4493 <jireh4493@gmail.com>
2023-04-20 21:55:22 +0900

    commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,20 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navigation = ({ userObj }) => {
-
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/profile">{userObj.displayName}'s Profile</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">{userObj.displayName}'s Profile</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
